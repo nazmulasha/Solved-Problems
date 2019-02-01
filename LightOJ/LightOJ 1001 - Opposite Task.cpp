10b249c76acd0740a8f0743da5c8fad9ccfba522
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 00:13:45 +0600

    Giveaway

---
+++ LightOJ/LightOJ 1001 - Opposite Task.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        int tmp=x/2;
+        cout<<tmp<<" "<<x-tmp<<endl;
+    }
+    return 0;
+}
