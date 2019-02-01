66e365ea52873c7d8bdd22b70381372092fbbb90
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 01:39:40 +0600

    basic

---
+++ Toph/The Optimus Three.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool comp(pair<int,int>p1,pair<int,int>p2)
+{
+    if(p1.first>p2.first)
+        return true;
+    else if(p1.first==p2.first)
+        return p1.second>p2.second;
+
+}
+
+bool cmp(int x,int y)
+{
+    return x>y;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<1<<" "<<1<<" "<<n-2<<endl;
+    return 0;
+}
