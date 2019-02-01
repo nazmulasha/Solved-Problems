2854adce7d931089cea27191e3c79803bc3454c4
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 01:11:55 +0600

    basic

---
+++ Toph/#Hashtag.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    for(int i=0;i<str.size();i++)
+    {
+        if(str[i]!=' ')
+            cout<<str[i];
+    }
+    cout<<endl;
+    return 0;
+}
