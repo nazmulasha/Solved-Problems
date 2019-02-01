38fec5d7679f6f360d65c2d16ee05126f29512f5
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 01:10:18 +0600

    basic

---
+++ Toph/Life of Decimals.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int arr[13]={0,1,4,1,5,9,2,6,5,3,5,8,9};
+        int n;
+        cin>>n;
+        cout<<arr[n]<<endl;
+
+    }
+
+    return 0;
+
+}
