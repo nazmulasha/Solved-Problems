cece7b5278ea0669bca59edef7888635fdbc7cf1
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 01:19:55 +0600

    geometry

---
+++ Toph/Meena Has Three Wishes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        double a,b;
+        cin>>a;
+        b=a*(a/2.0);
+        cout<<fixed<<setprecision(2);
+        cout<<b<<endl;
+    }
+
+    return 0;
+}
