0dd6e990ef6a09eb6b687610caf42eb5155dab1b
Nazmul Hasan <nazmul.rasna@northsouth.edu>
2019-02-02 01:29:10 +0600

    math

---
+++ Toph/Ha Ha Ha.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tt;
+    cin>>tt;
+    for(int t=1;t<=tt;t++)
+    {
+        long long int a,b;
+        cin>>a>>b;
+        cout<<"Case "<<t<<": "<<a*a+b*b<<endl;
+    }
+    return 0;
+
+}
